605da93cda780150177d26d084e3877dd0491e94
jukkanghost <jukkanghost@gmail.com>
2024-02-09 12:56:27 +0100

    SpringConfig

---
+++ BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/config/SpringConfig.java
@@ -0,0 +1,17 @@
+package com.banana.bananawhatsapp.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@ComponentScan(basePackages = {"com.banana.bananaWhatsapp.persistencia", "com.banana.bananaWhatsapp.servicios"})
+@PropertySource("classpath:application.properties")
+@EntityScan("com.banana.bananaWhatsapp.modelos")
+@EnableAutoConfiguration
+@EnableJpaRepositories("com.banana.bananaWhatsapp.persistencia")
+public class SpringConfig {
+}
